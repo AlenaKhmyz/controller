3a4d8d84882cc8e7e148d1682705117a0efffa73
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-10-24 17:09:11 +0300

    added a few elements for page of main

--- controller/src/SignIn/index.js
+++ controller/src/SignIn/index.js
@@ -3,26 +3,36 @@ import './style.css'
 import logo from './logo.png'
 
 function SignInPage() {
+  const [password, setPassword] = useState('')
+  const[login, setLogin] = useState('')
+
+  const onSave = () => {
+    return console.log(password, login)
+  }
 
   return(
     <div className="form">
       <img className="logo" src={ logo} alt="Логотип"/>
       <div className="phone">
-        <p className="label-phone">Логин</p>
         <input
         type="text"
         className="form-phone"
+        placeholder="Логин"
+        value={login} onChange={(event) => {setLogin(event.target.value)}}
         />
       </div>
       <div className="password">
-        <p className="label-password">Пароль</p>
         <input
         type="text"
         className="form-password"
+        placeholder="Пароль"
         type="password"
+        value={password} onChange={(event) => {setPassword(event.target.value)}}
         />
       </div>
-    <button className="form-button">Войти</button> 
+      <div className="button">
+        <button className="form-button" onClick={onSave}>Войти</button> 
+      </div>
     </div>
     
   )
