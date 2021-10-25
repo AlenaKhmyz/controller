f2159ab57df912d803b18bdc5e11fd2444d983bc
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-10-25 23:36:52 +0300

    switched to svelte

---
+++ controller/src/SignIn.svelte
@@ -0,0 +1,79 @@
+<!-- 1) подключение шрифтов -->
+
+<script>
+  let password = '';
+  let login = '';
+  let src = "./logo.png";
+  const onSave = () => {
+    return console.log(password, login)
+  }
+</script>
+
+<div class="form">
+  <img class="logo" {src} alt="Логотип"/>
+  <div class="phone">
+    <input
+    type="text"
+    class="form-phone"
+    placeholder="Логин"
+    value={login} onChange={(event) => {login(event.target.value)}}
+    />
+  </div>
+  <div class="password">
+    <input
+    class="form-password"
+    placeholder="Пароль"
+    type="password"
+    value={password} onChange={(event) => {password(event.target.value)}}
+    />
+  </div>
+  <div class="button">
+    <button class="form-button" onClick={onSave}>Войти</button> 
+  </div>
+</div>
+
+<style>
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  .phone, .password, .button, .logo {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 20rem;
+    margin-bottom: 10px;
+  }
+
+  .form-phone, .form-password {
+    width: 100%;
+    height: 2.2rem;
+    border: 1px solid grey;
+    border-radius: 5px;
+    padding: 0 5px;
+  }
+
+  .button {
+    display: flex;
+    justify-content: end;
+  }
+
+  .form-button {
+    width: 100%; 
+    height: 3rem;
+    font: 22px/1 'Malina', sans-serif;
+    letter-spacing: -.5px;
+    -webkit-font-smoothing: antialiased;
+    border: 1px solid rgb(36, 162, 201);
+    border-radius: 5px;
+    background-color: rgb(36, 162, 201);
+    color: rgb(255, 255, 255);
+  }
+
+</style>
+
+
---
+++ controller/src/main.js
@@ -0,0 +1,7 @@
+import SignIn from './SignIn.svelte';
+
+const app = new SignIn({
+	target: document.body
+});
+
+export default app;
