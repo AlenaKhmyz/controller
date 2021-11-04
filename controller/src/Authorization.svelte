31d1e2aeae89c6b5d1d01f40b9a1b009c61f055c
Alena <72409651+AlenaKhmyz@users.noreply.github.com>
2021-11-04 17:25:04 +0300

    added Settings page

---
+++ controller/src/Authorization.svelte
@@ -0,0 +1,38 @@
+
+
+
+<div class="settings">
+<!-- Блок авторизации -->
+  <div class="authorization">
+    <h2 class="authorization-data">Авторизационные данные</h2>
+    <div class="login">
+      <div class="current-login">
+        <p class="name-current-login"></p>
+        <input class="current-login-input-field">
+      </div>
+       <div class="new-login">
+        <p class="name-new-login"></p>
+        <input class="current-login-input-field">
+      </div>
+    </div>
+    <div class="password">
+      <div class="current-password">
+        <p class="name-current-password"></p>
+        <input class="current-password-input-field">
+      </div>
+      <div class="new-password">
+        <p class="name-new-password"></p>
+        <input class="current-password-input-field">
+      </div>
+      <div class="confirmation-password">
+        <p class="name-confirmation-password"></p>
+        <input class="confirmation-password-input-field">
+      </div>
+    </div>
+    <button class="authoraization-button">Подтвердить</button>
+  </div>
+<!-- Блок  настройки Wi-Fi -->
+  <div class="settings-wi-fi">
+    
+  </div>
+</div>
